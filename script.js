9ce1f42600686143e9119f8ed463b657c39fc54e
UMR_147 【ウマル】 <97790149+UMRwork@users.noreply.github.com>
2022-11-23 17:54:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+  function menu(){
+    location.replace("menu.html")
+  }
+  function home(){
+    location.replace("index.html")
+  }
+  function pay(){
+    location.replace("pay.html")
+  }
+  function payment(){
+    location.replace("payment.html")
+  }
+  function confirmation(){
+    location.replace("track.html")
+  }
